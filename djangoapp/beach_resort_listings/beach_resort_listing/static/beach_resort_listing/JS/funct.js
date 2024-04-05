9c531729f287599376ee8f70f496546177254684
victorcodebase <victorkithinji@outlook.com>
2024-04-05 11:17:50 +0300

    fixed search

--- djangoapp/beach_resort_listings/beach_resort_listing/static/beach_resort_listing/JS/funct.js
+++ djangoapp/beach_resort_listings/beach_resort_listing/static/beach_resort_listing/JS/funct.js
@@ -1,15 +1,10 @@
-console.log("Javascript connected");
-console.log("Viewport height: " + window.innerHeight);
-
 function openlisting(resortId) {
 	resortId = resortId - 2;
 	learn_more = document.getElementById("learn_more_btn");
 	rawResortData = learn_more.getAttribute("data-info");
 	resortJson = rawResortData.replace(/'/g, '"');
-	console.log(resortId);
 	let resort = JSON.parse(resortJson)[resortId]; // resorts start from 1, but array starts from 0
 
-	console.log(resort);
 	listing_card = document.getElementById("listing_card");
 	listing_card.style.display = "block";
 
@@ -18,15 +13,17 @@ function openlisting(resortId) {
 	resort_description = document.getElementById("resort_description");
 	resort_shots = document.getElementById("resort_shots");
 	resort_amenities = document.getElementById("resort_amenities");
+	resort_trending = document.getElementById("resort_trending");
 
 	resort_name.innerHTML = resort.name;
 	resort_image.src = resort.image;
 	resort_description.innerHTML = resort.desc;
+	resort_trending.innerHTML = `Trending at the ${resort.name} `;
+	resort;
 	amenityArray = resort.amenities.split(",");
 	for (i = 0; i < amenityArray.length; i++) {
 		amenity_div = document.createElement("div");
 		amenity_div.classList.add("border", "border-dark", "rounded", "amenity");
-		console.log("AMENITY", amenityArray[i]);
 
 		// icon = document.createElement("i");
 		// icon.classList.add(resort.amenityIcon[amenity], "amenity-icon");
@@ -36,7 +33,6 @@ function openlisting(resortId) {
 
 		// amenity_div.appendChild(icon);
 		amenity_div.appendChild(text);
-		console.log(amenity_div);
 
 		resort_amenities.appendChild(amenity_div);
 	}
@@ -60,13 +56,12 @@ function openlisting(resortId) {
 	}
 	const listingDisplayCard = document.getElementById("listing_card");
 	listingDisplayCard.style.top = `${scrollY + 40}px`;
-
 }
 
 function closelisting() {
 	console.log("Close listing clicked");
 	listing_card = document.getElementById("listing_card");
-	const elements_to_unstage = Array.from(document.getElementsByClassName("amenity"));
+	const elements_to_unstage = Array.from(document.querySelectorAll(".amenity #resort_trending"));
 	elements_to_unstage.forEach((element) => {
 		element.remove();
 	});
@@ -83,9 +78,13 @@ function search(search_or_cancel) {
 		cards = document.getElementsByClassName("card-component");
 		for (i = 0; i < cards.length; i++) {
 			card = cards[i];
+
 			card.style.display = "none";
-			if (card.innerHTML.toUpperCase().includes(input.value.toUpperCase())) {
+			const h2Element = card.querySelector("#component_card_title"); // Retrieve the title element
+			console.log("ELEMENT", h2Element.innerHTML);
+			if (h2Element.innerHTML.toUpperCase().includes(input.value.toUpperCase())) { // Check if the title contains the searched value
 				card.style.display = "block";
+				console.log("found")
 			}
 		}
 	} else if (search_or_cancel == "cancel") {
@@ -109,5 +108,5 @@ const images = [
 	"https://images.pexels.com/photos/3144978/pexels-photo-3144978.jpeg?auto=compress&cs=tinysrgb&w=600",
 	"https://images.pexels.com/photos/4577191/pexels-photo-4577191.jpeg?auto=compress&cs=tinysrgb&w=600",
 	"https://images.pexels.com/photos/2631613/pexels-photo-2631613.jpeg?auto=compress&cs=tinysrgb&w=600",
-	"https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg?auto=compress&cs=tinysrgb&w=600"
+	"https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg?auto=compress&cs=tinysrgb&w=600",
 ];
